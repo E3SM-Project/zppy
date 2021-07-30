9526ee7cb151e30c9f6470d98c2545f2fb09d0c1
Ryan Forsyth <forsyth2@llnl.gov>
2021-07-30 16:12:06 -0500

    Update E3SM Diags testing

--- tests/integration/test_bash_generation.py
+++ tests/integration/test_bash_generation.py
@@ -9,11 +9,11 @@ class TestBashGeneration(unittest.TestCase):
         )
         self.assertEqual(
             os.system(
-                "diff -bur test_output/post/scripts /lcrc/group/e3sm/public_html/zppy_test_resources/expected_bash_files"
+                "diff -bur test_bash_generation_output/post/scripts /lcrc/group/e3sm/public_html/zppy_test_resources/expected_bash_files"
             ),
             0,
         )
-        self.assertEqual(os.system("rm -r test_output"), 0)
+        self.assertEqual(os.system("rm -r test_bash_generation_output"), 0)
 
 
 if __name__ == "__main__":
