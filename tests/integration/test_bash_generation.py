0db825bf7d3ba743223c202efc0a8df60ce7d9a7
forsyth2 <30700190+forsyth2@users.noreply.github.com>
2021-10-15 12:20:34 -0700

    Ignore templateDir in tests (#153)

--- tests/integration/test_bash_generation.py
+++ tests/integration/test_bash_generation.py
@@ -9,7 +9,7 @@ class TestBashGeneration(unittest.TestCase):
         )
         self.assertEqual(
             os.system(
-                "diff -bur test_bash_generation_output/post/scripts /lcrc/group/e3sm/public_html/zppy_test_resources/expected_bash_files"
+                "diff -bur -I 'templateDir' test_bash_generation_output/post/scripts /lcrc/group/e3sm/public_html/zppy_test_resources/expected_bash_files"
             ),
             0,
         )
