664db99e3d12d9fa965c66dbcdf472038f745244
Ryan Forsyth <forsyth2@llnl.gov>
2022-04-29 12:10:12 -0500

    Model vs model improvements

--- zppy/e3sm_diags.py
+++ zppy/e3sm_diags.py
@@ -64,6 +64,29 @@ def e3sm_diags(config, scriptDir):  # noqa: C901
                     c["ref_year1"],
                     c["ref_year2"],
                 )
+                reference_data_path = (
+                    c["reference_data_path"].split("/post")[0] + "/post"
+                )
+                if c["reference_data_path_climo_diurnal"] == "":
+                    c[
+                        "reference_data_path_climo_diurnal"
+                    ] = f"{reference_data_path}/atm/{c['grid']}/clim_diurnal_8xdaily"
+                if c["reference_data_path_tc"] == "":
+                    c[
+                        "reference_data_path_tc"
+                    ] = f"{reference_data_path}/atm/tc-analysis_{c['ref_year1']}_{c['ref_year2']}"
+                if c["reference_data_path_ts"] == "":
+                    c[
+                        "reference_data_path_ts"
+                    ] = f"{reference_data_path}/atm/{c['grid']}/ts/monthly"
+                if c["reference_data_path_ts_rof"] == "":
+                    c[
+                        "reference_data_path_ts_rof"
+                    ] = f"{reference_data_path}/rof/native/ts/monthly"
+                if c["gauges_path"] == "":
+                    c[
+                        "gauges_path"
+                    ] = "/lcrc/group/e3sm/public_html/e3sm_diags_test_data/unit_test_complete_run/obs/time-series/GSIM/GSIM_catchment_characteristics_all_1km2.csv"  # Define with Mache?
             else:
                 raise ValueError("Invalid run_type={}".format(c["run_type"]))
             print(prefix)
