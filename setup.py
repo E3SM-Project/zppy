591061867e331068cbb166b0e68b93d04467652f
Ryan Forsyth <forsyth2@llnl.gov>
2022-06-24 19:48:38 -0500

    Bump to 2.1.0rc3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ data_files = package_files(
 
 setup(
     name="zppy",
-    version="2.1.0rc2",
+    version="2.1.0rc3",
     author="Ryan Forsyth, Chris Golaz",
     author_email="forsyth2@llnl.gov, golaz1@llnl.gov",
     description="Post-processing software for E3SM",
