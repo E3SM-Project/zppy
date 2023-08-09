cc4f92e095ae4787439088b676b8ed67a1d81914
Ryan Forsyth <forsyth2@llnl.gov>
2023-08-08 17:05:47 -0700

    Compy zppy dev with rc10 packages

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -160,7 +160,7 @@ def get_compy_expansions(config):
         # To run this test, replace conda environment with your e3sm_diags dev environment
         "diags_environment_commands": "source /qfs/people/fors729/miniconda3/etc/profile.d/conda.sh; conda activate e3sm_diags_dev_20230807",
         "diags_walltime": "03:00:00",
-        "environment_commands_test": "source /share/apps/E3SM/conda_envs/test_e3sm_unified_1.9.0rc9_compy.sh",
+        "environment_commands_test": "source /share/apps/E3SM/conda_envs/test_e3sm_unified_1.9.0rc10_compy.sh",
         "expected_dir": "/compyfs/www/zppy_test_resources/",
         "partition_long": "slurm",
         "partition_short": "short",
