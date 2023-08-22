33c119c9e007744ae90c4aea439615cf4aa992a1
Ryan Forsyth <forsyth2@llnl.gov>
2023-08-22 15:29:50 -0500

    Edit Chrysalis test generators

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -131,11 +131,11 @@ def get_chyrsalis_expansions(config):
     username = config.get("web_portal", "username")
     web_base_path = config.get("web_portal", "base_path")
     d = {
-        "bundles_walltime": "02:00:00",
+        "bundles_walltime": "07:00:00",
         "constraint": "",
         # To run this test, replace conda environment with your e3sm_diags dev environment
         "diags_environment_commands": "source /home/ac.forsyth2/miniconda3/etc/profile.d/conda.sh; conda activate e3sm_diags_20230816",
-        "diags_walltime": "2:00:00",
+        "diags_walltime": "5:00:00",
         "environment_commands_test": "source /lcrc/soft/climate/e3sm-unified/test_e3sm_unified_1.9.0rc12_chrysalis.sh",
         "expected_dir": "/lcrc/group/e3sm/public_html/zppy_test_resources/",
         "partition_long": "compute",
