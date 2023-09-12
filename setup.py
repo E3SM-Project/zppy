8a33b7014a01234a1adc7895f4cb3ffd2523f74c
forsyth2 <30700190+forsyth2@users.noreply.github.com>
2023-09-12 10:32:14 -0700

    Bump to 2.3.0 (#505)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ data_files = package_files(
 
 setup(
     name="zppy",
-    version="2.3.0rc6",
+    version="2.3.0",
     author="Ryan Forsyth, Chris Golaz",
     author_email="forsyth2@llnl.gov, golaz1@llnl.gov",
     description="Post-processing software for E3SM",
--- zppy/__init__.py
+++ zppy/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "v2.3.0rc6"
+__version__ = "v2.3.0"
