842343e2e07e8da674f7a7e1ecc04290f564c670
forsyth2 <30700190+forsyth2@users.noreply.github.com>
2023-09-15 12:31:19 -0700

    Test Unified 1.9.0 on Perlmutter (#509)

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -184,7 +184,7 @@ def get_perlmutter_expansions(config):
         "bundles_walltime": "6:00:00",
         "constraint": "cpu",
         # To run this test, replace conda environment with your e3sm_diags dev environment
-        "diags_environment_commands": "source /global/homes/f/forsyth/miniconda3/etc/profile.d/conda.sh; conda activate e3sm_diags_20230908",
+        "diags_environment_commands": "source /global/homes/f/forsyth/miniconda3/etc/profile.d/conda.sh; conda activate e3sm_diags_20230914",
         "diags_walltime": "6:00:00",
         "environment_commands_test": "source /global/common/software/e3sm/anaconda_envs/test_e3sm_unified_1.9.0rc16_pm-cpu.sh",
         "expected_dir": "/global/cfs/cdirs/e3sm/www/zppy_test_resources/",
