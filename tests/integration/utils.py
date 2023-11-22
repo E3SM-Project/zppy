aa1db7fcc378c75e8a43259657c062f6aa076adf
Ryan Forsyth <forsyth2@llnl.gov>
2023-11-22 12:43:48 -0600

    Test ts for land variables

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -7,7 +7,7 @@ from typing import List
 from mache import MachineInfo
 from PIL import Image, ImageChops, ImageDraw
 
-UNIQUE_ID = "unique_id"
+UNIQUE_ID = "issue_531"
 
 # Image checking ##########################################################
 
