e566351ce552cbf0b44ca1c5adf04b94611f1514
Ryan Forsyth <forsyth2@llnl.gov>
2023-11-28 08:38:57 -0600

    Add lat_lon_land set

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -7,7 +7,7 @@ from typing import List
 from mache import MachineInfo
 from PIL import Image, ImageChops, ImageDraw
 
-UNIQUE_ID = "unique_id"
+UNIQUE_ID = "issue_518"
 
 # Image checking ##########################################################
 
