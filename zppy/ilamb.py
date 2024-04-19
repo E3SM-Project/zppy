f53e916cb9105d80842cdaf00121458dee72eba1
forsyth2 <30700190+forsyth2@users.noreply.github.com>
2024-04-19 14:20:54 -0700

    Fix commands printing (#579)

--- zppy/ilamb.py
+++ zppy/ilamb.py
@@ -126,7 +126,7 @@ def ilamb(config, scriptDir, existing_bundles, job_ids_file):
                 else:
                     print("...adding to bundle '%s'" % (c["bundle"]))
 
-            print(f"environment_commands={c['environment_commands']}")
+            print(f"   environment_commands={c['environment_commands']}")
             print_url(c, "ilamb")
 
     return existing_bundles
