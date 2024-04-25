8fc9eac10dceb29fb13e651d1913672b675b97b6
Ryan Forsyth <forsyth2@llnl.gov>
2024-04-24 19:34:38 -0700

    Run test utils on Perlmutter for v2.5.0rc2

---
+++ tests/integration/generated/update_defaults_expected_files_pm-cpu.sh
@@ -0,0 +1,11 @@
+# Run this script to update expected files for test_defaults.py
+# Run from the top level of the zppy repo
+# Run as `./tests/integration/generated/update_defaults_expected_files.sh`
+
+rm -rf /global/cfs/cdirs/e3sm/www/zppy_test_resources/test_defaults_expected_files
+mkdir -p /global/cfs/cdirs/e3sm/www/zppy_test_resources/test_defaults_expected_files
+# Your output will now become the new expectation.
+# You can just move (i.e., not copy) the output since re-running this test will re-generate the output.
+mv test_defaults_output/post/scripts/*.settings /global/cfs/cdirs/e3sm/www/zppy_test_resources/test_defaults_expected_files
+# Rerun test
+python -u -m unittest tests/integration/test_defaults.py
