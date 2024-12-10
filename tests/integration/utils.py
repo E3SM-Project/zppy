a27781cb2a2ba34c1fb5ad731eafb530af5274eb
Ryan Forsyth <forsyth2@llnl.gov>
2024-12-10 11:02:02 -0600

    Clean up code

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -7,7 +7,7 @@ from typing import List
 from mache import MachineInfo
 from PIL import Image, ImageChops, ImageDraw
 
-UNIQUE_ID = "test-636-20241210"
+UNIQUE_ID = "unique_id"
 
 # Image checking ##########################################################
 
