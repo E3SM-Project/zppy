ee605603ea631b141e98992cba885e811a41f925
Ryan Forsyth <forsyth2@llnl.gov>
2024-12-20 14:40:20 -0600

    Use updated nco for zi input

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -7,7 +7,7 @@ from typing import List
 from mache import MachineInfo
 from PIL import Image, ImageChops, ImageDraw
 
-UNIQUE_ID = "unique_id"
+UNIQUE_ID = "zppy_601_viewers_updated_nco_no_extra_vars"
 
 # Image checking ##########################################################
 
