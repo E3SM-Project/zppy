4c9f0af8ab3fda0bf6e4d6563da923fbc8e44e95
Ryan Forsyth <forsyth2@llnl.gov>
2025-02-21 12:48:20 -0600

    Remove npo flag

--- zppy/templates/ts.bash
+++ zppy/templates/ts.bash
@@ -62,7 +62,7 @@ if grep -q "*" input.txt; then
 fi
 # Generate time series files
 # If the user-defined parameter "vars" is "", then ${vars}, defined above, will be too.
-cat input.txt | ncclimo --npo \
+cat input.txt | ncclimo \
 -c {{ case }} \
 {%- if vars != '' %}
 -v ${vars} \
