024c5686f2a25e906b48f5ac80fa5bddfb654b2e
Ryan Forsyth <forsyth2@llnl.gov>
2025-06-06 16:43:01 -0500

    Fix paths to plots

--- zppy/templates/global_time_series.bash
+++ zppy/templates/global_time_series.bash
@@ -6,12 +6,14 @@
 # Generate global time series plots
 ################################################################################
 {% if subsection != None %}
-results_dir={{ subsection }}/{{ prefix }}_results
-zi-global-time-series --subsection {{ subsection }} --use_ocn {{ use_ocn }} --input {{ input }} --input_subdir {{ input_subdir }} --moc_file {{ moc_file }} --case_dir {{ output }} --experiment_name {{ experiment_name }} --figstr {{ figstr }} --color {{ color }} --ts_num_years {{ ts_num_years }} --plots_original {{ plots_original }} --plots_atm {{ plots_atm }} --plots_ice {{ plots_ice }} --plots_lnd {{ plots_lnd }} --plots_ocn {{ plots_ocn }} --nrows {{ nrows }} --ncols {{ ncols }} --results_dir ${results_dir} --regions {{ regions }} --make_viewer {{ make_viewer }} --start_yr {{ year1 }} --end_yr {{ year2 }}
+subsection={{ subsection }}
 {%- else %}
-results_dir={{ prefix }}_results
-zi-global-time-series --subsection "no_subsection" --use_ocn {{ use_ocn }} --input {{ input }} --input_subdir {{ input_subdir }} --moc_file {{ moc_file }} --case_dir {{ output }} --experiment_name {{ experiment_name }} --figstr {{ figstr }} --color {{ color }} --ts_num_years {{ ts_num_years }} --plots_original {{ plots_original }} --plots_atm {{ plots_atm }} --plots_ice {{ plots_ice }} --plots_lnd {{ plots_lnd }} --plots_ocn {{ plots_ocn }} --nrows {{ nrows }} --ncols {{ ncols }} --results_dir ${results_dir} --regions {{ regions }} --make_viewer {{ make_viewer }} --start_yr {{ year1 }} --end_yr {{ year2 }}
+subsection="no_subsection"
 {%- endif %}
+
+results_dir={{ prefix }}_results
+zi-global-time-series --subsection ${subsection} --use_ocn {{ use_ocn }} --input {{ input }} --input_subdir {{ input_subdir }} --moc_file {{ moc_file }} --case_dir {{ output }} --experiment_name {{ experiment_name }} --figstr {{ figstr }} --color {{ color }} --ts_num_years {{ ts_num_years }} --plots_original {{ plots_original }} --plots_atm {{ plots_atm }} --plots_ice {{ plots_ice }} --plots_lnd {{ plots_lnd }} --plots_ocn {{ plots_ocn }} --nrows {{ nrows }} --ncols {{ ncols }} --results_dir ${results_dir} --regions {{ regions }} --make_viewer {{ make_viewer }} --start_yr {{ year1 }} --end_yr {{ year2 }}
+
 
 
 results_dir_absolute_path={{ scriptDir }}/${results_dir}
@@ -51,10 +53,7 @@ done
 {% endif %}
 
 # Copy files
-# TODO: Fix web paths
-# With top_level as dst: classic_original_8_no_ocn/global_time_series_classic_original_8_no_ocn_1985-1995_results/ -- this shows up empty
-# With results_level as dst: classic_original_8_no_ocn/global_time_series_classic_original_8_no_ocn_1985-1995_results/global_time_series_classic_original_8_no_ocn_1985-1995_results/ -- this path works, but has a repeat
-rsync -a --delete ${results_dir_absolute_path} ${results_level}
+rsync -a --delete ${results_dir_absolute_path} ${top_level}
 if [ $? != 0 ]; then
   cd {{ scriptDir }}
   echo 'ERROR (8)' > {{ prefix }}.status
