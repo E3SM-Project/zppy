5a8229e563820bb80894a7ad9466c143772174c9
ShixuanZhang <shixuan.zhang@pnnl.gov>
2026-05-11 14:27:54 -0700

    Ajust to allow enso metrics and revise enso vars to be identified by workflow

--- zppy/pcmdi_diags.py
+++ zppy/pcmdi_diags.py
@@ -56,10 +56,6 @@ def pcmdi_diags(config, script_dir, existing_bundles, job_ids_file):
             logger.warning(
                 "The 'enso' set is currently a testing mode in PCMDI Diags. Turn on it with care."
             )
-        #if c["current_set"] == "enso":
-        #    logger.warning(
-        #        "The 'enso' set is not yet supported in PCMDI Diags. Skipping launching of associated jobs."
-        #    )
         #    continue  # Skip this task
         c["sub"] = get_value_from_parameter(
             c, "subsection", "sub", ParameterInferenceType.SECTION_INFERENCE
